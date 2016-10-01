836dbce4d77ed8d26e257a822334674e7f643553
Shashank Kothapalli <kothapas@shashanks-macbook-pro-2.local>
2016-09-30 22:23:19 -0700

    Initial Commit

---
+++ MemeMe 1.0 The Meme Editor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe 1.0 The Meme Editor
+//
+//  Created by Kothapalli on 9/30/16.
+//  Copyright © 2016 Kothapalli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
